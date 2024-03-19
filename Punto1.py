84ae565429db353c4be52f7391ab3ceda9c2214d
HaroldJP <142249299+HaroldJP@users.noreply.github.com>
2024-03-19 14:04:27 -0500

    Update Punto1.py

--- Punto1.py
+++ Punto1.py
@@ -57,12 +57,42 @@ class Rectangle(Shape):
     def compute_vertices(self):
         return [self.vertice1.x, self.vertice1.y], [self.vertice2.x, self.vertice2.y], [self.vertice3.x, self.vertice3.y], [self.vertice4.x, self.vertice4.y]
 
+    def initialization_rectangle():
+        x = float(input("Enter the x coordinate for the rectangle's bottom-left corner: "))
+        y = float(input("Enter the y coordinate for the rectangle's bottom-left corner: "))
+        width = float(input("Enter the rectangle's width: "))
+        height = float(input("Enter the rectangle's height: "))
+
+        rectangle = Rectangle(True, x, y, x + width, y, x + width, y + height, x, y + height, #vertices
+        x, y, x + width, y, x + width, y, x + width, y + height, x + width, y + height, x , y + height, x, y + height, x, y, #edges
+        90, 90, 90, 90, #angles
+        width, height) #width, height
+
+        print("Area:", rectangle.compute_area())
+        print("Perimeter:", rectangle.compute_perimeter())
+        print("Inner Angles:", rectangle.compute_inner_angles())
+        print("Vertices:", rectangle.compute_vertices())
+
     #def is_inside(self, point):
         #return self.vertice1.x <= point.x <= self.vertice3.x and self.vertice1.y <= point.y <= self.vertice3.y
 
 class Square(Rectangle):
     def __init__(self, *args):
         super().__init__(*args)
+
+    def initialization_square():
+        x = float(input("Enter the x coordinate for the square's bottom-left corner: "))
+        y = float(input("Enter the y coordinate for the square's bottom-left corner: "))
+        width = float(input("Enter the square's width: "))
+        height = width
+        square = Square(True, x, y, x + width, y, x + width, y + height, x, y + height, #vertices
+        x, y, x + width, y, x + width, y, x + width, y + height, x + width, y + height, x , y + height, x, y + height, x, y, #edges
+        90, 90, 90, 90, #angles
+        width, height) #width, height
+        print("Area:", square.compute_area())
+        print("Perimeter:", square.compute_perimeter())
+        print("Inner Angles:", square.compute_inner_angles())
+        print("Vertices:", square.compute_vertices())
 
 class Triangle(Shape):
     def __init__(self, *args):
@@ -93,105 +123,79 @@ class Isosceles(Triangle):
     def __init__(self, *args):
         super().__init__(*args)
 
+    def initialization_isosceles_triangle():
+        x = float(input("Enter the x coordinate for the isosceles triangle's bottom-left corner: "))
+        y = float(input("Enter the y coordinate for the isosceles triangle's bottom-left corner: "))
+        base_length = float(input("Enter the isosceles triangle's base length: "))
+        equal_side_length = float(input("Enter the isosceles triangle's equal side length: "))
+        isosceles_triangle = Isosceles(False, x, y, x + base_length, y, (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), 0, 0,
+            x, y, x + base_length, y, x + base_length, y, (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), x, y, 0, 0, 0, 0, 
+            0, 0, 0, 0, 
+            base_length, math.sqrt(equal_side_length**2 - (base_length / 2)**2))
+        print("Area:", isosceles_triangle.compute_area())
+        print("Perimeter:", isosceles_triangle.compute_perimeter())
+        print("Inner Angles:", isosceles_triangle.compute_inner_angles())
+        print("Vertices:", isosceles_triangle.compute_vertices())
+
 class Equilateral(Triangle):
     def __init__(self, *args):
         super().__init__(*args)
+
+    def initialization_equilateral_triangle():
+        x = float(input("Enter the x coordinate for the equilateral triangle's bottom-left corner: "))
+        y = float(input("Enter the y coordinate for the equilateral triangle's bottom-left corner: "))
+        side_length = float(input("Enter the equilateral triangle's side length: "))
+        equilateral_triangle = Equilateral(True, x, y, x + side_length, y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, 0, 0,
+            x, y, x + side_length, y, x + side_length, y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, x, y, 0, 0, 0, 0,
+            60, 60, 60, 0,
+            side_length, (side_length**2 - (side_length / 2)**2)**(1/2))
+        print("Perimeter:", equilateral_triangle.compute_perimeter())
+        print("Area:", equilateral_triangle.compute_area())
+        print("Inner Angles:", equilateral_triangle.compute_inner_angles())
+        print("Vertices:", equilateral_triangle.compute_vertices())
 
 class Scalene(Triangle):
     def __init__(self, *args):
         super().__init__(*args)
 
+    def initialization_scalene_triangle():
+        x1 = float(input("Enter the x coordinate for the scalene triangle's first vertice: "))
+        y1 = float(input("Enter the y coordinate for the scalene triangle's first vertice: "))
+        x2 = float(input("Enter the x coordinate for the scalene triangle's second vertice: "))
+        y2 = float(input("Enter the y coordinate for the scalene triangle's second vertice: "))
+        x3 = float(input("Enter the x coordinate for the scalene triangle's third vertice: "))
+        y3 = float(input("Enter the y coordinate for the scalene triangle's third vertice: "))
+        scalene_triangle = Scalene(False, x1, y1, x2, y2, x3, y3, 0, 0,
+            x1, y1, x2, y2, x2, y2, x3, y3, x3, y3, x1, y1, 0, 0, 0, 0,
+            0, 0, 0, 0,
+            math.sqrt((x2 - x1)**2 + (y2 - y1)**2), 0)
+        print("Area:", scalene_triangle.compute_area())
+        print("Perimeter:", scalene_triangle.compute_perimeter())
+        print("Inner Angles:", scalene_triangle.compute_inner_angles())
+        print("Vertices:", scalene_triangle.compute_vertices())
+
 class TriRectangle(Triangle):
     def __init__(self, *args):
         super().__init__(*args)
 
-def initialization_rectangle():
-    x = float(input("Enter the x coordinate for the rectangle's bottom-left corner: "))
-    y = float(input("Enter the y coordinate for the rectangle's bottom-left corner: "))
-    width = float(input("Enter the rectangle's width: "))
-    height = float(input("Enter the rectangle's height: "))
-
-    rectangle = Rectangle(True, x, y, x + width, y, x + width, y + height, x, y + height, #vertices
-    x, y, x + width, y, x + width, y, x + width, y + height, x + width, y + height, x , y + height, x, y + height, x, y, #edges
-    90, 90, 90, 90, #angles
-    width, height) #width, height
-
-    print("Area:", rectangle.compute_area())
-    print("Perimeter:", rectangle.compute_perimeter())
-    print("Inner Angles:", rectangle.compute_inner_angles())
-    print("Vertices:", rectangle.compute_vertices())
-
-def initialization_square():
-    x = float(input("Enter the x coordinate for the square's bottom-left corner: "))
-    y = float(input("Enter the y coordinate for the square's bottom-left corner: "))
-    width = float(input("Enter the square's width: "))
-    height = width
-    square = Square(True, x, y, x + width, y, x + width, y + height, x, y + height, #vertices
-    x, y, x + width, y, x + width, y, x + width, y + height, x + width, y + height, x , y + height, x, y + height, x, y, #edges
-    90, 90, 90, 90, #angles
-    width, height) #width, height
-    print("Area:", square.compute_area())
-    print("Perimeter:", square.compute_perimeter())
-    print("Inner Angles:", square.compute_inner_angles())
-    print("Vertices:", square.compute_vertices())
-
-def initialization_equilateral_triangle():
-    x = float(input("Enter the x coordinate for the triangle's bottom-left corner: "))
-    y = float(input("Enter the y coordinate for the triangle's bottom-left corner: "))
-    side_length = float(input("Enter the equilateral triangle's side length: "))
-    equilateral_triangle = Equilateral(True, x, y, x + side_length, y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, 0, 0,
-        x, y, x + side_length, y, x + side_length, y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, (x + side_length) - (side_length / 2), (side_length**2 - (((x + side_length) - side_length / 2) - (x + side_length))**2)**(1/2) + y, x, y, 0, 0, 0, 0,
-        60, 60, 60, 0,
-        side_length, (side_length**2 - (side_length / 2)**2)**(1/2))
-    print("Perimeter:", equilateral_triangle.compute_perimeter())
-    print("Area:", equilateral_triangle.compute_area())
-    print("Inner Angles:", equilateral_triangle.compute_inner_angles())
-    print("Vertices:", equilateral_triangle.compute_vertices())
-
-def initialization_tri_rectangle():
-    x = float(input("Enter the x coordinate for the triangle rectangle's bottom-left corner: "))
-    y = float(input("Enter the y coordinate for the triangle rectangle's bottom-left corner: "))
-    width = float(input("Enter the triangle rectangle's width: "))
-    height = float(input("Enter the triangle rectangle's height: "))
-    tri_rectangle = TriRectangle(False, x, y, x + width, y, x, y + height, 0, 0,
-        x, y, x + width, y, x + width, y, x, y + height, x, y + height, x, y, 0, 0, 0, 0,
-        0, 0, 0, 0,
-        width, height)
-    print("Area:", tri_rectangle.compute_area())
-    print("Perimeter:", tri_rectangle.compute_perimeter())
-    print("Inner Angles:", tri_rectangle.compute_inner_angles())
-    print("Vertices:", tri_rectangle.compute_vertices())
-
-def initialization_scalene_triangle():
-    x1 = float(input("Enter the x coordinate for the first vertex: "))
-    y1 = float(input("Enter the y coordinate for the first vertex: "))
-    x2 = float(input("Enter the x coordinate for the second vertex: "))
-    y2 = float(input("Enter the y coordinate for the second vertex: "))
-    x3 = float(input("Enter the x coordinate for the third vertex: "))
-    y3 = float(input("Enter the y coordinate for the third vertex: "))
-    scalene_triangle = Scalene(False, x1, y1, x2, y2, x3, y3, x1, y1, x2, y2, x3, y3, math.degrees(math.acos((math.pow(scalene_triangle.edge1.length, 2) + math.pow(scalene_triangle.edge2.length, 2) - math.pow(scalene_triangle.edge3.length, 2)) / (2 * scalene_triangle.edge1.length * scalene_triangle.edge2.length))), math.degrees(math.acos((math.pow(scalene_triangle.edge1.length, 2) + math.pow(scalene_triangle.edge3.length, 2) - math.pow(scalene_triangle.edge2.length, 2)) / (2 * scalene_triangle.edge1.length * scalene_triangle.edge3.length))), math.degrees(math.acos((math.pow(scalene_triangle.edge2.length, 2) + math.pow(scalene_triangle.edge3.length, 2) - math.pow(scalene_triangle.edge1.length, 2)) / (2 * scalene_triangle.edge2.length * scalene_triangle.edge3.length))), scalene_triangle.edge1.length, scalene_triangle.edge2.length, scalene_triangle.edge3.length)
-    print("Area:", scalene_triangle.compute_area())
-    print("Perimeter:", scalene_triangle.compute_perimeter())
-    print("Inner Angles:", scalene_triangle.compute_inner_angles())
-
-def initialization_isosceles_triangle():
-    x = float(input("Enter the x coordinate for the triangle's bottom-left corner: "))
-    y = float(input("Enter the y coordinate for the triangle's bottom-left corner: "))
-    base_length = float(input("Enter the triangle's base length: "))
-    equal_side_length = float(input("Enter the triangle's equal side length: "))
-    isosceles_triangle = Isosceles(False, x, y, x + base_length, y, (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), 0, 0,
-        x, y, x + base_length, y, x + base_length, y, (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), (x + base_length) - ( base_length / 2), y + math.sqrt((equal_side_length)**2 - ((x + base_length) - ( x + base_length - (base_length / 2)))**2), x, y, 0, 0, 0, 0, 
-        0, 0, 0, 0, 
-        base_length, math.sqrt(equal_side_length**2 - (base_length / 2)**2))
-    print("Area:", isosceles_triangle.compute_area())
-    print("Perimeter:", isosceles_triangle.compute_perimeter())
-    print("Inner Angles:", isosceles_triangle.compute_inner_angles())
-    print("Vertices:", isosceles_triangle.compute_vertices())
+    def initialization_tri_rectangle():
+        x = float(input("Enter the x coordinate for the triangle rectangle's bottom-left corner: "))
+        y = float(input("Enter the y coordinate for the triangle rectangle's bottom-left corner: "))
+        width = float(input("Enter the triangle rectangle's width: "))
+        height = float(input("Enter the triangle rectangle's height: "))
+        tri_rectangle = TriRectangle(False, x, y, x + width, y, x, y + height, 0, 0,
+            x, y, x + width, y, x + width, y, x, y + height, x, y + height, x, y, 0, 0, 0, 0,
+            0, 0, 0, 0,
+            width, height)
+        print("Area:", tri_rectangle.compute_area())
+        print("Perimeter:", tri_rectangle.compute_perimeter())
+        print("Inner Angles:", tri_rectangle.compute_inner_angles())
+        print("Vertices:", tri_rectangle.compute_vertices())
 
 if __name__ == "__main__":
-    #initialization_rectangle()
-    #initialization_square()
-    #initialization_equilateral_triangle()
-    #initialization_tri_rectangle()
-    #initialization_scalene_triangle()
-    initialization_isosceles_triangle()
+    Rectangle.initialization_rectangle()
+    Square.initialization_square()
+    Equilateral.initialization_equilateral_triangle()
+    TriRectangle.initialization_tri_rectangle()
+    Isosceles.initialization_isosceles_triangle()
+    Scalene.initialization_scalene_triangle()
